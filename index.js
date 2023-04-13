c8c9d9999702fa5d4e453f0d0611a11c5968b174
CJ Bauzon <72855434+cbauzon@users.noreply.github.com>
2023-04-13 16:16:01 -0700

    Allow app to connect to local database

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /* Libraries Used */
 const express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    $ = require("jquery");
 
-
-
+mongoose.connect("mongodb://127.0.0.1:27017/loginPage");
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+const User = mongoose.model("User", userSchema);
 
 const app = express();
 app.use(express.urlencoded({extended: true}));     
@@ -16,9 +22,21 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.find({email: email}).then((foundUsers) => {
+        console.log(foundUsers);
+        if (foundUsers.length != 0) {
+            console.log("Found a user!");
+        } else {
+            console.log("Failed to find user :(");
+        }
+    });
+
+    res.redirect("/");
 
 });
-
 
 
 app.listen(process.env.PORT || 3000, () => {
