91092fcc0cb3c999453295db87bb30f785635920
CJ Bauzon <72855434+cbauzon@users.noreply.github.com>
2023-04-13 21:40:10 -0700

    Add list of online individuals after login

--- index.js
+++ index.js
@@ -3,35 +3,65 @@ const express = require("express"),
     mongoose = require("mongoose"),
     $ = require("jquery");
 
+/* Setting up Mongoose */
 mongoose.connect("mongodb://127.0.0.1:27017/loginPage");
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    status: Number
 });
 const User = mongoose.model("User", userSchema);
 
+/* Setting up Express */
 const app = express();
 app.use(express.urlencoded({extended: true}));     
 app.use(express.static("public"));      // all static elements stored in public
 app.set("view engine", "ejs");          // using ejs as view engine
 
+/* Global Variables */
+let email = "";
+let password = "";
+let fullName = "";
 
+/* Routes */
 app.get("/", (req, res) => {
     res.render("login", {incorrect: 0});
-})
+});
+
+app.get("/status", (req, res) => {
+    if (email) {
+        User.find({status: 1}).then((userList) => {
+            res.render("status", {user: fullName, userList: userList});
+        });
+    } else {
+        res.redirect("/");
+    }
+});
 
 app.post("/", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    email = req.body.email;
+    password = req.body.password;
+
+    authUser(email, password, res);
+});
+
+
+/* Helper Functions */
+const authUser = (email, password, res) => {
     let incorrect = 0;
 
-    User.find({email: email}).then((foundUsers) => {
-        console.log(foundUsers);
+    User.find({email: email}).then((userList) => {
+        console.log(userList);
 
         // Logic for checking for user and password
-        if (foundUsers.length === 1 && foundUsers[0].password === password) {
+        if (userList.length === 1 && userList[0].password === password) {
             console.log("Correct");
+            fullName = userList[0].name;
+            User.updateOne({email: userList[0].email}, {status: 1}).then(() => {
+                console.log("Update successful!");
+            });
+            res.redirect("/status");
         } else {
             incorrect = 1;
             console.log("Incorrect");
@@ -39,10 +69,13 @@ app.post("/", (req, res) => {
         }
         
     });
+}
 
-});
+const checkStatus = () => {
+    
+}
 
-
+/* Start App */
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is up!");
 });
