39c2eb5c6d6c88a23477e3e6b5b85f900dc30baf
CJ Bauzon <72855434+cbauzon@users.noreply.github.com>
2023-04-13 20:03:36 -0700

    Create user auth logic

--- index.js
+++ index.js
@@ -18,23 +18,27 @@ app.set("view engine", "ejs");          // using ejs as view engine
 
 
 app.get("/", (req, res) => {
-    res.render("login");
+    res.render("login", {incorrect: 0});
 })
 
 app.post("/", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
+    let incorrect = 0;
 
     User.find({email: email}).then((foundUsers) => {
         console.log(foundUsers);
-        if (foundUsers.length != 0) {
-            console.log("Found a user!");
-        } else {
-            console.log("Failed to find user :(");
-        }
-    });
 
-    res.redirect("/");
+        // Logic for checking for user and password
+        if (foundUsers.length === 1 && foundUsers[0].password === password) {
+            console.log("Correct");
+        } else {
+            incorrect = 1;
+            console.log("Incorrect");
+            res.render("login", {incorrect: incorrect});
+        }
+        
+    });
 
 });
 
